99cc3b18db4789a9fa4df396be9bfdb58d34bca4
pvslvch <polina.vasilevichh@gmail.com>
2025-07-25 13:21:21 +0200

    feat[playlists]: implement logic for handling request status and errors

--- src/features/playlists/playlists.tsx
+++ src/features/playlists/playlists.tsx
@@ -4,17 +4,36 @@ import { useQuery } from "@tanstack/react-query";
 import cls from "./playlists.module.css";
 
 export const Playlists = () => {
-  const fetchPlaylists = () => client.GET("/playlists");
+  const fetchPlaylists = async () => {
+    const response = await client.GET("/playlists");
 
-  const { data: playlists } = useQuery({
+    if (response.error) {
+      throw (response as unknown as { error: Error }).error;
+    }
+
+    return response.data!;
+  };
+
+  const {
+    data: playlists,
+    isPending,
+    isFetching,
+    isError,
+    error,
+  } = useQuery({
     queryKey: ["playlists"],
     queryFn: fetchPlaylists,
   });
 
+  if (isPending) return <span>Loading...</span>;
+
+  if (isError) return <span>{error.message}</span>;
+
   return (
     <div className={cls.playlists}>
+      {isFetching ? "⏳" : ""}
       <ul>
-        {playlists?.data?.data.map((playlist) => (
+        {playlists.data.map((playlist) => (
           <li key={playlist.id}>{playlist.attributes.title}</li>
         ))}
       </ul>
