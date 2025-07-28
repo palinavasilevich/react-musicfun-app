a18378de0f9ca8695ba394da60b88926b90e7789
pvslvch <polina.vasilevichh@gmail.com>
2025-07-28 16:24:18 +0200

    feat(auth): implement user authentication

---
+++ src/features/auth/ui/LoginButton/LoginButton.tsx
@@ -0,0 +1,62 @@
+import { useMutation } from "@tanstack/react-query";
+
+import { client } from "../../../../shared/api/client";
+
+import cls from "./LoginButton.module.css";
+
+export const LoginButton = () => {
+  const callbackUrl = "http://localhost:5173/oauth/callback";
+
+  const { mutate } = useMutation({
+    mutationFn: async ({ code }: { code: string }) => {
+      const response = await client.POST("/auth/login", {
+        body: {
+          code,
+          redirectUri: callbackUrl,
+          rememberMe: true,
+          accessTokenTTL: "1d",
+        },
+      });
+
+      if (response.error) {
+        throw new Error(response.error.message);
+      }
+
+      return response.data;
+    },
+  });
+
+  const handleLoginClick = () => {
+    window.addEventListener("message", handleOauthMessage);
+
+    window.open(
+      `https://musicfun.it-incubator.app/api/1.0/auth/oauth-redirect?callbackUrl=${callbackUrl}`,
+      "apihub-oauth2",
+      "width=500, height=500"
+    );
+  };
+
+  const handleOauthMessage = (event: MessageEvent) => {
+    window.removeEventListener("message", handleOauthMessage);
+
+    if (event.origin !== document.location.origin) {
+      console.warn("origin mot match");
+      return;
+    }
+
+    const code = event.data.code;
+
+    if (!code) {
+      console.warn("no code in message");
+      return;
+    }
+
+    mutate({ code });
+  };
+
+  return (
+    <button className={cls.loginButton} onClick={handleLoginClick}>
+      Login with APIHUB
+    </button>
+  );
+};
--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from "@tanstack/react-router";
 
 import { Header } from "./Header";
+import { LoginButton } from "../features/auth/ui/LoginButton";
 
 import cls from "./RootLayout.module.css";
 
 export const RootLayout = () => (
   <>
-    <Header renderAccountBar={() => <div>Account</div>} />
+    <Header renderAccountBar={() => <LoginButton />} />
     <main className={cls.main}>
       <Outlet />
     </main>
